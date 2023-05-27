67ddc2a590476b283425378d5584ec42e0ddaa5c
ltpbje <19836788252@163.com>
2023-05-27 17:30:54 +0800

    1

--- AJAX/2-原生AJAX/server.js
+++ AJAX/2-原生AJAX/server.js
@@ -11,6 +11,22 @@ app.get('/server', (request, response) => {
     // 设置响应体
     response.send('HELLO AJAX');
 });
+
+app.post('/server', (request, response) => {
+    // 设置响应头，设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    // 设置响应体
+    response.send('HELLO AJAX POST');
+});
+
+app.all('/server', (request, response) => {
+    // 设置响应头，设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    // 设置响应体
+    response.send('HELLO AJAX POST');
+});
+
 // 4.监听端口启动服务
 app.listen(8000, () => {
     console.log("服务已启动，8000端口监听中.......");
