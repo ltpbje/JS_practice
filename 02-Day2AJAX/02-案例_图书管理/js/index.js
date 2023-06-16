1c13888709d152998190c1416f583acaa1f62111
ltpbje <19836788252@163.com>
2023-06-16 16:27:44 +0800

    1

--- 02-Day2AJAX/02-案例_图书管理/js/index.js
+++ 02-Day2AJAX/02-案例_图书管理/js/index.js
@@ -13,7 +13,29 @@ function getBooklist() {
         }
     }).then(result => {
         console.log(result);
+        console.log(result.data.data);
+        bookList = result.data.data;
+        const str_bookList = bookList.map((item, index) => {
+            const { bookname, author, publisher } = item;
+            return `<tr>
+              <td>${index + 1}</td>
+              <td>${bookname}</td>
+              <td>${author}</td>
+              <td>${publisher}</td>
+              <td>
+                <span class="del">删除</span>
+                <span class="edit">编辑</span>
+              </td>
+            </tr>`;
+        }).join('');
+        document.querySelector('.list').innerHTML = str_bookList;
     })
 
 };
 getBooklist();
+const add_btn = document.querySelector('.add-btn');
+const Modal = document.querySelector('.modal-content');
+const addModal = new bootstrap.Modal(Modal);
+add_btn.addEventListener('click', () => {
+    addModal.hide();
+});
