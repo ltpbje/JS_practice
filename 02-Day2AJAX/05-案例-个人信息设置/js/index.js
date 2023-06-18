8e2a408d96396b11efc73f554591db39d60d6594
ltpbje <19836788252@163.com>
2023-06-18 20:51:14 +0800

    1

--- 02-Day2AJAX/05-案例-个人信息设置/js/index.js
+++ 02-Day2AJAX/05-案例-个人信息设置/js/index.js
@@ -3,12 +3,13 @@
  *  1.1 获取用户的数据
  *  1.2 回显数据到标签上
  * */
+const creator = 'bo仔';
 // 数据回显
 axios({
     url: 'http://hmajax.itheima.net/api/settings',
     method: 'get',
     params: {
-        creator: '播仔',
+        creator
     }
 }).then(result => {
     console.log(result);
@@ -17,7 +18,27 @@ axios({
     Object.keys(useData).forEach(key => {
         if (key === 'avatar') {
             document.querySelector('.prew').src = useData[key];
+        } else if (key === 'gender') {
+            document.querySelector(`.gender[value='${useData[key]}']`).checked = true;
         }
-
+        else {
+            document.querySelector(`.${key}`).value = useData[key];
+        }
     });
 })
+// 修改头像
+document.querySelector('.upload').addEventListener('change', function (e) {
+    console.log(e.target.files);
+    const fd = new FormData();
+    fd.append('avatar', e.target.files[0]);
+    fd.append('creator', creator);
+    axios({
+        url: 'http://hmajax.itheima.net/api/avatar',
+        method: 'put',
+        data: fd
+    }).then(result => {
+        // console.log(result.data.data.avatar);
+        const imgUrl = result.data.data.avatar
+        document.querySelector('.prew').src = imgUrl;
+    })
+});
