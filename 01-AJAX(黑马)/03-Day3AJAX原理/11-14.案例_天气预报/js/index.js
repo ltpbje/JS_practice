744ddcc8a205e1b2fefda5391ffbb4825e17ca62
ltpbje <19836788252@163.com>
2023-06-21 21:05:04 +0800

    1

--- 01-AJAX(黑马)/03-Day3AJAX原理/11-14.案例_天气预报/js/index.js
+++ 01-AJAX(黑马)/03-Day3AJAX原理/11-14.案例_天气预报/js/index.js
@@ -5,22 +5,22 @@
  */
 // 获取天气信息函数
 function getWeather(cityCode) {
-    myAxios({
-        url: 'http://hmajax.itheima.net/api/weather',
-        params: {
-            city: cityCode
-        }
-    }).then(result => {
-        const dataObj = result.data;
-        console.log(dataObj);
-        // title区域
-        const titleStr = ` <span class="dateShort">${dataObj.dateShort}</span>
+  myAxios({
+    url: 'http://hmajax.itheima.net/api/weather',
+    params: {
+      city: cityCode
+    }
+  }).then(result => {
+    const dataObj = result.data;
+    console.log(dataObj);
+    // title区域
+    const titleStr = ` <span class="dateShort">${dataObj.dateShort}</span>
         <span class="calendar">农历&nbsp;
           <span class="dateLunar">${dataObj.dateLunar}</span>
         </span>`
-        document.querySelector('.title').innerHTML = titleStr
-        // weather-box区域
-        const weatherStr = ` <div class="tem-box">
+    document.querySelector('.title').innerHTML = titleStr
+    // weather-box区域
+    const weatherStr = ` <div class="tem-box">
         <span class="temp">
           <span class="temperature">${dataObj.temperature}</span>
           <span>°</span>
@@ -40,10 +40,10 @@ function getWeather(cityCode) {
           <li class="windPower">2级</li>
         </ul>
       </div>`;
-        document.querySelector('.weather-box').innerHTML = weatherStr;
-        // today-weather 今日天气区域
-        const twObj = dataObj.todayWeather;
-        const twStr = `<div class="range-box">
+    document.querySelector('.weather-box').innerHTML = weatherStr;
+    // today-weather 今日天气区域
+    const twObj = dataObj.todayWeather;
+    const twStr = `<div class="range-box">
         <span>今天：</span>
         <span class="range">
           <span class="weather">${twObj.weather}</span>
@@ -71,11 +71,11 @@ function getWeather(cityCode) {
           <span class="sunsetTime">${twObj.sunsetTime}</span>
         </li>
       </ul>`;
-        document.querySelector('.today-weather').innerHTML = twStr;
-        // 周天气预报 week-weather-box
-        const forecastArr = dataObj.dayForecast;
-        const forecastStr = forecastArr.map(item => {
-            return `<li class="item">
+    document.querySelector('.today-weather').innerHTML = twStr;
+    // 周天气预报 week-weather-box
+    const forecastArr = dataObj.dayForecast;
+    const forecastStr = forecastArr.map(item => {
+      return `<li class="item">
           <div class="date-box">
             <span class="dateFormat">${item.dateFormat}</span>
             <span class="date">${item.date}</span>
@@ -92,12 +92,30 @@ function getWeather(cityCode) {
             <span class="windPower">${item.windPower}</span>
           </div>
         </li>`
-        }).join('');
-        document.querySelector('.week-wrap').innerHTML = forecastStr;
-    }).catch(error => {
-        console.dir(error);
-    });
-
+    }).join('');
+    document.querySelector('.week-wrap').innerHTML = forecastStr;
+    // 城市名
+    document.querySelector('.area').innerHTML = dataObj.area;
+  }).catch(error => {
+    console.dir(error);
+  });
 
 }
 getWeather(110100);
+// 搜索城市列表
+document.querySelector('.search-city').addEventListener('input', function (e) {
+  console.log(e.target.value);
+  myAxios({
+    url: 'http://hmajax.itheima.net/api/weather/city',
+    params: {
+      city: e.target.value
+    }
+  }).then(result => {
+    // console.log(result.data);
+    const cityArr = result.data;
+    const liListStr = cityArr.map(item => {
+      return `<li class="city-item">${item.name}</li>`
+    }).join('');
+    document.querySelector('.search-list').innerHTML = liListStr;
+  })
+});
