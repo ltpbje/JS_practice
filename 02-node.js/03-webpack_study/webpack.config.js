ef9e117ac2322e5476c970d8ffbb6737e141ffac
ltpbje <19836788252@163.com>
2023-07-23 13:06:44 +0800

    1

--- 02-node.js/03-webpack_study/webpack.config.js
+++ 02-node.js/03-webpack_study/webpack.config.js
@@ -62,3 +62,19 @@ module.exports = {
         ],
     },
 }
+var config = {
+    entry: './app.js',
+    //...
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+    }
+
+    if (argv.mode === 'production') {
+        //...
+    }
+
+    return config;
+};
